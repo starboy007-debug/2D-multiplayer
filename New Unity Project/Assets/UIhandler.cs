c93e202c1101f4c79fd4d90eee48ffb70140d2df
starboy007-debug <65112799+starboy007-debug@users.noreply.github.com>
2020-10-25 21:08:15 +0530

    creating room

---
+++ New Unity Project/Assets/UIhandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+public class UIhandler : MonoBehaviourPunCallbacks
+{
+    public InputField createRoomTF;
+    public InputField joinRoomTF;
+
+    public void OnClick_JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinRoomTF.text,null);
+    }
+
+    public void OnClick_CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createRoomTF.text,new RoomOptions {MaxPlayers = 4},null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Room joined sucessfully");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        print("Room joined Failed "+returnCode+" message "+message);
+
+    }
+
+}
